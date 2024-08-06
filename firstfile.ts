0fb436347313b04cfe1fe84cde3f53af9fe50109
sunbulnaz <161361478+sunbulnaz@users.noreply.github.com>
2024-08-06 17:14:08 +0500

    push my day8 challange

---
+++ firstfile.ts
@@ -0,0 +1,30 @@
+// ### Day 8: TypeScript Modules (Export and Import)
+
+// #### Question 1: Exporting and Importing Multiple Functions
+
+// *Question:*
+// Create two separate files. In the first file, define and export two functions: calculateArea (calculates the area of a rectangle given its width and height) and calculatePerimeter (calculates the perimeter of a rectangle given its width and height). In the second file, import both functions and use them to calculate the area and perimeter of a rectangle with a width of 5 and a height of 10. Print the results to the console.
+
+// *Hint:*
+// - Use export to export the functions in the first file.
+// - Use named imports to bring the functions into the second file.
+// - Ensure that the file paths are correctly specified when importing.
+
+//Answer
+//First file: Export two functions CalculateArea and CalculatePerimeter
+
+export function  calculateArea(width:number,height:number):number{
+    return width*height
+}
+
+
+
+
+
+
+
+
+
+export function  calculatePerimeter(width:number,height:number):number{
+    return width*height
+}
--- secondfile.ts
+++ secondfile.ts
@@ -6,3 +6,11 @@
 // console.log(calculateArea(5,10))
 
 // console.log(calculatePerimeter(5,10))
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/sumbulNaz8/1.git
+// git push -u origin main
