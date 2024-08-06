624ab9095d4e63eb7288d3c133532ad7d1ea9f06
sunbulnaz <161361478+sunbulnaz@users.noreply.github.com>
2024-08-06 17:07:44 +0500

    push secondfile

---
+++ secondfile.js
@@ -0,0 +1,6 @@
+"use strict";
+//Second file: import both function 
+Object.defineProperty(exports, "__esModule", { value: true });
+const firstfile_1 = require("./firstfile");
+console.log((0, firstfile_1.calculateArea)(5, 10));
+console.log((0, firstfile_1.calculatePerimeter)(5, 10));
---
+++ secondfile.ts
@@ -0,0 +1,8 @@
+
+//Second file: import both function 
+
+//import { calculateArea,calculatePerimeter } from  './firstfile.js'
+
+// console.log(calculateArea(5,10))
+
+// console.log(calculatePerimeter(5,10))
