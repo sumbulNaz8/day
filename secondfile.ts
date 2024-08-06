62c8077aa0b35ce09b6e73b0ed1d32db16c7f615
Sumbul Naz <164656538+sumbulNaz8@users.noreply.github.com>
2024-08-06 17:17:53 +0500

    Update secondfile.ts

--- secondfile.ts
+++ secondfile.ts
@@ -1,16 +1,10 @@
 
 //Second file: import both function 
 
-//import { calculateArea,calculatePerimeter } from  './firstfile.js'
+import { calculateArea,calculatePerimeter } from  './firstfile.js'
 
-// console.log(calculateArea(5,10))
+ console.log(calculateArea(5,10))
 
-// console.log(calculatePerimeter(5,10))
+ console.log(calculatePerimeter(5,10))
 
 
-// git init
-// git add README.md
-// git commit -m "first commit"
-// git branch -M main
-// git remote add origin https://github.com/sumbulNaz8/1.git
-// git push -u origin main
